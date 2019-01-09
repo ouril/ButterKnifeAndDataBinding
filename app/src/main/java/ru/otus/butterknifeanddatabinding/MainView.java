cdfc8e3322bd9694f94b8ab2d35c60c2a654c3c6
ouril <ourilkregg@gmail.com>
2019-01-09 15:37:44 +0300

    finish

--- app/src/main/java/ru/otus/butterknifeanddatabinding/MainView.java
+++ app/src/main/java/ru/otus/butterknifeanddatabinding/MainView.java
@@ -2,13 +2,20 @@ package ru.otus.butterknifeanddatabinding;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.annotation.Nullable;
 import android.support.constraint.ConstraintLayout;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.util.Patterns;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,27 +33,137 @@ public class MainView extends ConstraintLayout {
     @BindView(R.id.button)
     Button button;
 
+    static final String NAME = "NAME";
+    static final String EMAIL = "EMAIL";
+    static final String PHONE = "PHONE";
 
     public MainView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         LayoutInflater.from(context).inflate(R.layout.main_view, this, true);
         ButterKnife.bind(this, this);
-        // add callback
+        final Pattern pattern = Patterns.EMAIL_ADDRESS;
+
+        TextMatcherCallback emailMatcher = new TextMatcherCallback() {
+            @Override
+            public boolean match(CharSequence s) {
+                if(pattern.matcher(s).matches()) return true;
+                return false;
+            }
+        };
+
+        TextMatcherCallback nameMatcher = new TextMatcherCallback() {
+            @Override
+            public boolean match(CharSequence s) {
+                if (s.length() > 0) return true;
+                return false;
+            }
+        };
+
+        TextGotCallback getName = buildCallBack(nameMatcher);
+        TextGotCallback getEmail = buildCallBack(emailMatcher);
+        TextGotCallback getPhone = new TextGotCallback() {
+            @Override
+            public void textGot(CharSequence s, final EditText editText, EditText nextEdit) {
+                if (validPhoneNumber(s)){
+                    editText.setOnKeyListener(new OnKeyListener() {
+                        @Override
+                        public boolean onKey(View v, int keyCode, KeyEvent event) {
+                            if (keyCode == KeyEvent.KEYCODE_ENTER){
+                                editText.setEnabled(false);
+                                button.setEnabled(true);
+                                button.setBackgroundColor(Color.GREEN);
+                                return true;
+                            }
+                            return false;
+                        }
+                    });
+                }
+            }
+        };
+
+
+        edit_name.setEnabled(true);
+        edit_name.requestFocus();
+        setListener(edit_name, edit_mail, getName);
+        setListener(edit_mail, edit_phone, getEmail);
+        setListener(edit_phone, null, getPhone);
+
+
     }
+
+    private void setListener(final EditText editText, @Nullable final EditText nextEdit, final TextGotCallback callback) {
+        editText.setFocusableInTouchMode(true);
+        editText.requestFocus();
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                callback.textGot(s, editText, nextEdit);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
     @OnClick(R.id.button)
     protected void onButtonClick() {
         if (edit_mail.getText().length() > 0 && edit_name.getText().length() > 0 &&
                 edit_phone.getText().length() > 0) {
             Intent intent = new Intent(getContext(), PersonActivity.class);
-            intent.putExtra("NAME", edit_name.getText());
-            intent.putExtra("PHONE", edit_phone.getText());
-            intent.putExtra("EMAIL", edit_mail.getText());
+            intent.putExtra(NAME, edit_name.getText().toString());
+            intent.putExtra(PHONE, edit_phone.getText().toString());
+            intent.putExtra(EMAIL, edit_mail.getText().toString());
             getContext().startActivity(intent);
 
         }
     }
 
-    interface MainCallback {
-        void openPerson();
+    private TextGotCallback buildCallBack(final TextMatcherCallback textMatcher){
+        return new TextGotCallback() {
+            @Override
+            public void textGot(CharSequence s, final EditText editText, final EditText nextEdit) {
+                if (textMatcher.match(s)){
+
+                    editText.setOnKeyListener(new OnKeyListener() {
+                        @Override
+                        public boolean onKey(View v, int keyCode, KeyEvent event) {
+                            if (keyCode == KeyEvent.KEYCODE_ENTER){
+                                nextEdit.setEnabled(true);
+                                editText.setEnabled(false);
+                                nextEdit.requestFocus();
+                                return true;
+                            }
+                            return false;
+                        }
+                    });
+                }
+            }
+        };
     }
+
+    private boolean validPhoneNumber(CharSequence s){
+        if (s.length() > 10 && s.toString().matches("^[0-9]+")) {
+            return true;
+        }
+        return false;
+    }
+
+    interface TextMatcherCallback {
+        boolean match(CharSequence s);
+    }
+
+
+
+
+    interface TextGotCallback {
+        void textGot(CharSequence s, EditText editText, EditText nextEdit);
+    }
+
 }
--- app/src/main/java/ru/otus/butterknifeanddatabinding/data_binding/PersonView.java
+++ app/src/main/java/ru/otus/butterknifeanddatabinding/data_binding/PersonView.java
@@ -3,11 +3,14 @@ package ru.otus.butterknifeanddatabinding.data_binding;
 import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
+
+import ru.otus.butterknifeanddatabinding.MainView;
 import ru.otus.butterknifeanddatabinding.R;
 import ru.otus.butterknifeanddatabinding.databinding.PersonViewExBinding;
 
@@ -19,11 +22,17 @@ public class PersonView extends LinearLayout {
         super(context, attrs);
         binding = DataBindingUtil.inflate(
                 LayoutInflater.from(context), R.layout.person_view_ex, this, true);
-
+        PersonActivity activity = (PersonActivity) context;
+        Bundle extras = activity.getIntent().getExtras();
+        if (extras != null){
+            bind(new Person(extras.getString("NAME"),
+                    extras.getString("EMAIL"), extras.getString("PHONE")));
+        }
 
     }
 
     public void bind(Person person){
+
         binding.setPerson(person);
     }
 
